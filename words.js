a4df5f80bfc4395ad1b7c26cde5ad3a7e2ef57a7
JoyKin <59808768+Foxyboy835@users.noreply.github.com>
2025-05-25 21:24:05 -0400

    Create words.js

---
+++ words.js
@@ -0,0 +1,5 @@
+const wordLists = {
+  4: ["room", "chef", "quiz", "milk", "gift", "spin"],
+  5: ["style", "magic", "plush", "donut", "candy", "socks", "click", "badge", "frame"],
+  6: ["gogogo", "deluxe", "sweety", "trophy", "kinzap", "gemmix", "mirror", "glitzy"]
+};
